8ed9d4a3f6dcd8e7dcfa36600d94ef21af39fa48
Gevorg <gevorg.kara@yandex.ru>
2021-12-16 17:09:21 +0300

    Сделал урок 4

---
+++ src/tutorial-4/app.js
@@ -0,0 +1,34 @@
+let tasks = ['Я хочу сделать список задач', 'Вторая заметка'];
+
+const input = document.querySelector("input[type='text']");
+const tasksList = document.querySelector('.tasks');
+const addBtn = document.querySelector('.addTask');
+
+const handleClickAdd = () => {
+    if (input.value === '' || !input.value.trim()) {
+        alert('Поле не должно быть пустым');
+    } else {
+        tasks = [...tasks, input.value];
+        render();
+        input.value = '';
+    }
+};
+
+const handleClickRemove = (index) => {
+    tasks.splice(index, 1);
+    render();
+};
+
+const render = () => {
+    tasksList.innerHTML = '';
+    tasks.forEach((task, index) => {
+        tasksList.innerHTML += `
+        <li>${task} <button onclick="handleClickRemove(${index})">x</button></li>
+        `;
+    });
+    console.log(tasks);
+};
+
+addBtn.addEventListener('click', handleClickAdd);
+
+render();
