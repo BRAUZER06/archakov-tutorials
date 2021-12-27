8aa0995c9a861153e72a20f14dce22770ba3e9c6
Gevorg <gevorg.kara@yandex.ru>
2021-12-27 18:45:55 +0300

    Сделал урок №8

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 
 // import App from './tutorial-1/App';
@@ -7,12 +8,15 @@ import './index.css';
 // import App from './tutorial-3/App';
 // import App from './tutorial-5/App';
 // import App from './tutorial-6/App';
-import App from './tutorial-7/App';
+// import App from './tutorial-7/App';
+import App from './tutorial-8/App';
 
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Router>
+            <App />
+        </Router>
     </React.StrictMode>,
     document.getElementById('root'),
 );
