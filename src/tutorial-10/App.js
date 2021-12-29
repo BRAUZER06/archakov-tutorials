63c334c80503687fe7f84af9e4c6f4f3e1278ec6
Gevorg <gevorg.kara@yandex.ru>
2021-12-29 17:09:52 +0300

    Сделал урок №10

---
+++ src/tutorial-10/App.js
@@ -0,0 +1,62 @@
+import axios from 'axios';
+import React from 'react';
+import useDebounce from './components/useDebounce';
+import './App.css';
+import Form from './components/Form';
+import Profile from './components/Profile';
+
+const App = () => {
+    const [value, setValue] = React.useState('');
+    const [profile, setProfile] = React.useState([]);
+    const [isLoading, setIsLoading] = React.useState(false);
+
+    const debouncedSearch = useDebounce(value, 500);
+
+    const getProfile = async () => {
+        try {
+            setIsLoading(true);
+            const { data } = await axios.get(`https://api.github.com/users/${value}`);
+            setProfile(data);
+            setIsLoading(false);
+        } catch (error) {
+            alert(`Пользователь ${value} не найден`);
+            setIsLoading(false);
+        }
+    };
+
+    const checkValue = () => {
+        if (value.trim() !== '') {
+            getProfile();
+        } else {
+            setValue('');
+            alert('Поле поиска не должно быть пустым');
+        }
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        checkValue();
+    };
+
+    React.useEffect(() => {
+        if (debouncedSearch) {
+            checkValue();
+        }
+    }, [debouncedSearch]);
+
+    return (
+        <div id="app">
+            <div className="app-container">
+                <Form
+                    value={value}
+                    setValue={setValue}
+                    handleSubmit={handleSubmit}
+                    isLoading={isLoading}
+                />
+                {profile.login && <Profile profile={profile} />}
+            </div>
+        </div>
+    );
+};
+
+export default App;
